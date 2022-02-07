7d7be22458311340efbe1fc7fed577f2184b3a7a
Maria Helena Carvalho <marianenacarvalho@gmail.com>
2022-02-07 00:27:32 -0300

    adicionando o arquivo de teste de códigos do javascript

---
+++ introducao-ao-javascript/teste.js
@@ -0,0 +1,15 @@
+//  teste
+
+/* teste */
+
+var preco = 2;
+var desconto = 0.2;
+var valor = preco - preco * desconto;
+
+console.log (valor);
+
+function soma(a, b) {;
+    return a + b;
+}
+
+console.log(soma(3, 5));
